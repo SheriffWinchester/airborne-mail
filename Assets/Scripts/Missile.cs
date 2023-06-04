1cd9979d5f362be4e6e987ed6c58380326966f99
SheriffKovalski <m3llpak@gmail.com>
2023-06-04 23:13:25 +0300

    Updated

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Missile : MonoBehaviour
 {
     public float speed = 3f;
+    public float rotationSpeed = 3f;
     Rigidbody2D rb;
     GameObject target;
     void Start() {
@@ -15,11 +16,12 @@ public class Missile : MonoBehaviour
         FollowTarget();
     }
     void FollowTarget() {
-        Vector2 vectorToTarget = target.transform.position - transform.position;
+        Vector2 vectorToTarget = transform.position - target.transform.position;
         Vector3 rotatedVectorToTarget = Quaternion.Euler(0, 0, 90) * vectorToTarget;
         Quaternion targetRotation = Quaternion.LookRotation(forward: Vector3.forward, upwards: rotatedVectorToTarget);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, speed * Time.deltaTime);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         rb.velocity = -transform.right * speed;
+        Debug.Log(targetRotation);
         // float angle = Mathf.Atan2(target.transform.position.y, target.transform.position.x) * Mathf.Rad2Deg;
         // instance.transform.Rotate(180, 0, -angle);
     }
